81b4411680353302c18a48662bfec38e63f88cf7
ViplavBilla <156365246+ViplavBilla@users.noreply.github.com>
2024-10-12 19:51:34 -0500

    updated login page by giving navigations to the  dashboard activity

--- app/src/main/java/com/example/attendance_buddy/MainActivity.java
+++ app/src/main/java/com/example/attendance_buddy/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.attendance_buddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void loginActivity(View v){
+        Button loginBTN = findViewById(R.id.loginBTN);
+        Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+        startActivity(intent);
+
     }
 }
