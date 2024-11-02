6012b1ec290d77adb291698f4a85e7028d9bec9e
ViplavBilla <156365246+ViplavBilla@users.noreply.github.com>
2024-11-01 19:33:45 -0500

    updated the attendance app with new activities and changing old activities

--- app/src/main/java/com/example/attendance_buddy/MainActivity.java
+++ app/src/main/java/com/example/attendance_buddy/MainActivity.java
@@ -1,34 +1,31 @@
 package com.example.attendance_buddy;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button startBTN = findViewById(R.id.startBTN);
+        startBTN.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onClickstartBTN(v);
+            }
         });
-
     }
-    public void loginActivity(View v){
-        Button loginBTN = findViewById(R.id.loginBTN);
-        Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
-        startActivity(intent);
 
+    public void onClickstartBTN(View v) {
+        Intent ini = new Intent(this, login.class);
+
+        startActivity(ini);
     }
 }
